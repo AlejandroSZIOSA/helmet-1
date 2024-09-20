82fccc54d451155c301c6a44bf478450f80d60cc
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-09-20 16:50:04 +0200

    working on robot txt

---
+++ src/scripts/robot-txt-generator.js
@@ -0,0 +1,18 @@
+//7 generate robot.txt
+/* const fs = require("fs"); */
+
+import fs from "fs";
+
+const generateRobotsTxt = () => {
+  const environment = process.env.NODE_ENV;
+  let content = "User-agent: */n";
+  if (environment === "production") {
+    content = "User-agent: */n";
+  } else {
+    content += "Disallow:*/n";
+  }
+  fs.writeFileSync("./public/js/robots.txt", content);
+};
+generateRobotsTxt();
+
+//"generate-robots": "node ./src/scripts/robot-txt-generator.js"
